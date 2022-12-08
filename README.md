f537bbd80f547ae2845d2d76aa8b380bbc64d955
samuel ezadmanesh <samezad27@gmail.com>
2022-12-07 19:04:22 -0800

    added logic to create HTML

--- README.md
+++ README.md
@@ -21,7 +21,7 @@ SO THAT I have quick access to their emails and GitHub profiles
 
 ```md
 GIVEN a command-line application that accepts user input
-WHEN I am prompted for my team members and their information
+WHEN I am prompted for my team members and their information -- 
 THEN an HTML file is generated that displays a nicely formatted team roster based on user input
 WHEN I click on an email address in the HTML
 THEN my default email program opens and populates the TO field of the email with the address
--- index.js
+++ index.js
@@ -1,35 +1,133 @@
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const inquirer = require("inquirer");
+const fs = require("fs");
+const employees = [];
 
-const Manager = require('./lib/Manager');
-const Engineer = require('./lib/Engineer');
-const Intern = require('./lib/Intern');
-const inquirer = require('inquirer');
-const fs = require('fs');
+// const
 
 //TODO - write your inquirer app here to gather information about the team members, and generate the HTML file using fs
-inquirer.prompt()([
-    {
-        type: 'list',
-        name: 'What position is this employee?',
-        choices: [
-            'Manager',
-            'Intern',
-            'Engineer',
+function newEmployee() {
+  console.log("hello");
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "position",
+        message: "What position is this employee?",
+        choices: ["Manager", "Intern", "Engineer"],
+      },
+      {
+        type: "input",
+        name: "name",
+        message: "What is the name of the employee?",
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "What is the email of the employee?",
+      },
+      {
+        type: "input",
+        name: "id",
+        message: "What is the id of the employee?",
+      },
+    ])
+    .then(({ position, email, id, name }) => {
+      switch (position) {
+        case "Manager":
+          //ask about office number
+          inquirer
+            .prompt([
+              {
+                type: "input",
+                name: "officeNumber",
+                message: "What is the office number?",
+              },
+            ])
+            .then(({ officeNumber }) => {
+              employees.push(new Manager(name, id, email, officeNumber));
 
-        ]
-    }
-]).then(({ position })=> {
-    switch(position) {
-        case 'Manager':
-            //ask about office number
+              another();
+            });
+          break;
 
-        case 'Intern': 
-            //ask about school
+        case "Intern":
+          //ask about school
+          break;
 
-        case 'Engineer':
-            //ask about github
+        case "Engineer":
+          //ask about github
+          inquirer
+          .prompt([
+            {
+              type: "input",
+              name: "github",
+              message: "What is your github?",
+            },
+          ])
+          .then(({ github }) => {
+            employees.push(new Engineer(name, id, email, github));
 
-        default: 
-    }
+            another();
+          });
+        break;
+
+        default:
+      }
+    });
+}
+
+function another() {
+  inquirer
+    .prompt([
+      {
+        type: "confirm",
+        name: "more",
+        message: "Create another?",
+      },
+    ])
+    .then(({ more }) => {
+      if (more) newEmployee();
+      else renderHTMLFile();
+    });
+}
+
+function renderHTMLFile() {
+  fs.writeFileSync(
+    "./index.html",
+    `
+    
 
 
-})
+    <ul>
+    
+
+        ${employees.map(
+          (employee) => `
+            <li>${employee.getRole()}</li>
+            
+        `
+        )}
+
+        ${employees.map(
+          (employee) => `
+            <li>${employee.getName()}</li>
+        `
+        )}
+
+        
+        ${employees.filter((employee)=>employee.getRole() === "Engineer").map(
+          (employee) => `
+        <li>${employee.getGithub()}</li>
+        `
+
+      )}
+        
+    </ul>
+    `
+  );
+}
+
+newEmployee();
